5927c7615be007529f2bca782ed41b4f37cfdab7
Zemian Deng <zemiandeng@gmail.com>
2022-09-20 07:56:57 -0400

    Adding contact form

---
+++ my-wp-content/themes/my-theme/lib/contact.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Process a contact us form using custom code together with a page template.
+ *
+ * The trigger/entry point of process_form() is kicked off only when the template is starting to render.
+ * See "page-contact.php" template file for details.
+ *
+ * NOTE: WordPress has many reserved query parameters names that you can not use. Hence, it's safer to
+ * prefix all form field to ensure its uniqueness. Example, use "mt_" in this contact form.
+ * See https://codex.wordpress.org/WordPress_Query_Vars
+ */
+namespace MyTheme\Contact;
+
+function process_form($form_data): array
+{
+    $form = ['errors' => [], 'data' => $form_data];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (validate_form($form) === 0) {
+            // Process form data here.
+            $form['message'] = ['text' => "Thank you for using our contact form!", 'type' => 'is-success'];
+            $form['data'] = []; // Clear form fill after process.
+        }
+    }
+    return $form;
+}
+
+/** Sanitize and validate form data */
+function validate_form(&$form): int
+{
+    $form_errors = &$form['errors'];
+    $form_data = &$form['data'];
+
+    $form_data['mt_name'] = htmlspecialchars(filter_var($form_data['mt_name'] ?? '', FILTER_SANITIZE_STRING));
+    if (filter_var($form_data['mt_name'], FILTER_VALIDATE_REGEXP,
+            ['options' => ['regexp' => '/.{1,100}/']]) === false) {
+        $form_errors['mt_name'] = 'Invalid name';
+    }
+
+    $form_data['mt_email'] = htmlspecialchars(filter_var($form_data['mt_email'] ?? '', FILTER_SANITIZE_EMAIL));
+    if (filter_var($form_data['mt_email'], FILTER_VALIDATE_EMAIL) === false) {
+        $form_errors['mt_email'] = 'Invalid email';
+    }
+
+    $form_data['mt_message'] = htmlspecialchars(filter_var($form_data['mt_message'] ?? '', FILTER_SANITIZE_STRING));
+    if (filter_var($form_data['mt_message'], FILTER_VALIDATE_REGEXP,
+            ['options' => ['regexp' => '/.{1,500}/']]) === false) {
+        $form_errors['mt_message'] = 'Invalid message';
+    }
+
+    return count($form_errors);
+}
